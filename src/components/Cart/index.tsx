87d225bca18850198fa8f3d72b8d0fd0809746e2
rickdouglas <rickmiranda23@gmail.com>
2023-10-01 04:10:33 -0300

    feat: carregando os produtos adicionados ao carrinho e calculando o valor total da compra

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,16 +1,49 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { CartItem } from "../CartItem";
 import "./cart.scss";
 
 export function Cart() {
+  const [cartProducts, setCartProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const existingCartProductsString = localStorage.getItem("cart");
+    const existingCartProducts = existingCartProductsString
+      ? JSON.parse(existingCartProductsString)
+      : [];
+    setCartProducts(existingCartProducts);
+
+    const cartTotal = existingCartProducts.reduce(
+      (acc, curr) => acc + parseFloat(curr.price),
+      0
+    );
+    setTotal(cartTotal);
+    console.log(cartTotal, "useefect");
+  }, []);
+
+  console.log(total);
+  console.log(cartProducts);
+
   return (
     <div className="cart__wrapper">
       <div className="cart__list">
-        <CartItem name="chinelo" price={35.99} />
-        <CartItem name="chinelo" price={35.99} />
-        <CartItem name="chinelo" price={35.99} />
-        <CartItem name="chinelo" price={35.99} />
+        {cartProducts.map((product, index) => (
+          <CartItem key={index} name={product.name} price={product.price} />
+        ))}
       </div>
-      <div className="cart__total">150</div>
+      <div className="cart__total">
+        Valor Total da Compra:
+        <b>
+          {total !== undefined
+            ? total.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })
+            : "0"}
+        </b>
+      </div>
     </div>
   );
 }
